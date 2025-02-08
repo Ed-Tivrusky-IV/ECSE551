fa52c9dd70c442e1602003c4fd45bc018138cd85
Ed-Tivrusky-IV <lycwllrlf0423@gmail.com>
2025-02-08 18:21:48 -0500

    update the test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from re import X
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,6 +9,24 @@ def load_data(file_path):
     df_cleaned = df.dropna(axis=1)
     print(f"Dimension of the {file_path} is:")
     print(df_cleaned.shape)
+    
+    #encode the label
+    df_cleaned["label"] = df_cleaned["label"].map({"CKD": 1, "Normal": 0})
+    
+    #df_cleaned = df_cleaned.drop(columns=["label"])
+    print("First 5 samples:")
+    print(df_cleaned.head())
+    
+    #convert df to numpy
+    np_array = df_cleaned.to_numpy()
+    
+    #split the features and the label
+    X = np_array[:,:-1]
+    y = np_array[:,-1]
+    return X,y
+
+
+    
     
 
 #Running within script
